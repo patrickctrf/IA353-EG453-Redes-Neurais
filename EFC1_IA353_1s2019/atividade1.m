c94ad0de58aed897dbd0fa54028b8faf542dc4a8
patrickctrf <patrickctrf@hotmail.com.br>
2019-04-09 04:26:38 -0300

    Backup Erros e Acertos
    Já consegue calcular corretamente o classificador e a quantidade de erros e acertos para cada um

--- EFC1_IA353_1s2019/atividade1.m
+++ EFC1_IA353_1s2019/atividade1.m
@@ -1,57 +1,61 @@
-clear all;
+% clear all;
+% 
+% % Carrega as matrizes X e S (Y, no roteiro) de entrada.
+% load('data.mat');
+% 
+% % O coeficiente de regularizacao, que varia de 2^-10 a 2^+10 com incremento
+% % multiplicativo de 2^2.
+% lambda=1;
+% % Temos 21 matrizes W (uma para cada coeficiente de regularizacao, todas de
+% % 784x10 elementos.
+% W = zeros(785, 10, 21);
+% % O vetor de bias que sera concatenado na matriz X e W para nao sei o que.
+% vetorExtraDe1 = ones(60000,1);
+% 
+% % Concatenando
+% X = [vetorExtraDe1, X];
+% 
+% %==========CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO=============
+% 
+% i=0;
+% while i < 21
+%     
+%     % Multiplicamos lambda por 4 == 2^2, a cada iteracao.
+%     lambda = 2^(i*2-10)
+%     
+%     % A equacao utilizada para realizar os minimos quadrados eh dada e
+%     % explicada no roteiro.
+%     W(:,:, i+1) = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
+%     
+%     i = i+1;    
+% end
+% 
+% % W = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
+% 
+% %==========FIM DO CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO======
 
-% Carrega as matrizes X e S (Y, no roteiro) de entrada.
-load('data.mat');
-
-% O coeficiente de regularizacao, que varia de 2^-10 a 2^+10 com incremento
-% multiplicativo de 2^2.
-lambda=1;
-% Temos 21 matrizes W (uma para cada coeficiente de regularizacao, todas de
-% 784x10 elementos.
-W = zeros(785, 10, 21);
-% O vetor de bias que sera concatenado na matriz X e W para nao sei o que.
-vetorExtraDe1 = ones(60000,1);
-
-% Concatenando
-X = [vetorExtraDe1, X];
-
-%==========CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO=============
-
-i=0;
-while i < 21
+erros = zeros(1,21);
+acertos = zeros(1,21);
+j=1;
+while j<=21
     
-    % Multiplicamos lambda por 4 == 2^2, a cada iteracao.
-    lambda = 2^(i*2-10)
-    
-    % A equacao utilizada para realizar os minimos quadrados eh dada e
-    % explicada no roteiro.
-    W(:,:, i+1) = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
-    
-    i = i+1;    
-end
+    erros(j) = 0;
+    acertos(j) = 0;
+    i = 40000 + 1;
+    while i<=60000
 
-% W = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
+        resultadoClassificacao = W(:,:,j)'*X(i,:)';
 
-%==========FIM DO CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO======
+        [~, indiceMaxResuladoClassificacao] = max(resultadoClassificacao);
+        [~, indiceMaxS] = max(S(i,:));
 
-erros = 0;
-acertos = 0;
-i = 40000 + 1;
-while i<=60000
-    
-    resultadoClassificacao = W(:,:,12)'*X(i,:)';
-    
-    [~, indiceMaxResuladoClassificacao] = max(resultadoClassificacao);
-    [~, indiceMaxS] = max(S(i,:));
-    
-    if indiceMaxS == indiceMaxResuladoClassificacao
-        acertos = acertos + 1;
-    else
-        erros = erros + 1;
+        if indiceMaxS == indiceMaxResuladoClassificacao
+            acertos(j) = acertos(j) + 1;
+        else
+            erros(j) = erros(j) + 1;
+        end
+        i = i + 1;
     end
-    
-    i = i + 1;
-    
+    j = j +1;
 end
 
-% W(:,:,1)'*X(1,:)'
