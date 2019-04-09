8bdf9b7efdc63853a75776627e9514e761d519db
patrickctrf <patrickctrf@hotmail.com.br>
2019-04-09 05:32:49 -0300

    Backup busca refinada completa
    Faz a busca refinada e encontra o minimo para as taxas de erro e acerto. Falta agora imprimir os graficos e gerar as saidas pedidas pelo roteiro.

--- EFC1_IA353_1s2019/atividade1.m
+++ EFC1_IA353_1s2019/atividade1.m
@@ -33,6 +33,11 @@
 % % W = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
 % 
 % %==========FIM DO CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO======
+% 
+
+
+
+%=============TAXAS DE ERROS E ACERTOS=====================================
 
 erros = zeros(1,21);
 acertos = zeros(1,21);
@@ -60,8 +65,89 @@ while j<=21
         i = i + 1;
     end
     
-    erroQuadratico(j) = erros(j);
+    erroQuadratico(j) = erros(j)^2;
     taxaDeAcertos(j) = acertos(j)/(acertos(j)+erros(j));
     j = j +1;
 end
+
+[~,melhorResultadoErroQuadratico ] = min(erroQuadratico);
+[~,melhorResultadoTaxaDeAcertos ] = max(taxaDeAcertos);
+
+% plot(1:21, erroQuadratico, 1:21, taxaDeAcertos*5*10^7);
+
+%=============FIM DE TAXAS DE ERROS E ACERTOS==============================
+
+
+
+
+
+%==========REFINADO CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO====
+
+W_refinado = zeros(785, 10, 21);
+
+% Faremos uma varredura em torno do minimo encontrado. Escolheremos o valor
+% do coeficiente anterior a aquele que produziu o melhor resultado e iremos
+% avancando de 2^0.2, multiplicativamente, ate alcancar o coeficiente de
+% classificacao seguinte ao de melhor resultado.
+i = melhorResultadoTaxaDeAcertos - 1 -1;% Lembre que o "i" comecava em zero
+                                        % na primeira varredura, entao
+                                        % devemos descontar "-1" novamente,
+                                        % pois os indices dos vetores em
+                                        % matlab comecam em "1" (nao "0") e
+                                        % queremos deixar centrado no meio
+                                        % o coeficiente que gerou o melhor
+                                        % resultado antes de refinar.
+j=0;
+while j < 21
+    
+    % Multiplicamos lambda por 2^0.2 a cada iteracao.
+    lambda = 2^(i*2-10 + j*0.2)
+    
+    % A equacao utilizada para realizar os minimos quadrados eh dada e
+    % explicada no roteiro.
+    W_refinado(:,:, j+1) = ((X(1:40000,:)'*X(1:40000,:)+lambda*eye(785))^-1)*X(1:40000,:)'*S(1:40000,:);
+    
+    j = j+1;
+end
+%===FIM DO REFINADO CALCULANDO W PARA CADA COEFICIENTE DE REGULARIZACAO====
+
+%=============TAXAS DE ERROS E ACERTOS=====================================
+
+erros = zeros(1,21);
+acertos = zeros(1,21);
+taxaDeAcertos = zeros(1,21);
+erroQuadratico = zeros(1,21);
+j=1;
+while j<=21
+    
+    erros(j) = 0;
+    acertos(j) = 0;
+    i = 40000 + 1;
+    while i<=60000
+
+        resultadoClassificacao = W_refinado(:,:,j)'*X(i,:)';
+
+        [~, indiceMaxResuladoClassificacao] = max(resultadoClassificacao);
+        [~, indiceMaxS] = max(S(i,:));
+
+        if indiceMaxS == indiceMaxResuladoClassificacao
+            acertos(j) = acertos(j) + 1;
+        else
+            erros(j) = erros(j) + 1;
+        end
+        
+        i = i + 1;
+    end
+    
+    erroQuadratico(j) = erros(j)^2;
+    taxaDeAcertos(j) = acertos(j)/(acertos(j)+erros(j));
+    j = j +1;
+end
+
+[~,melhorResultadoErroQuadratico ] = min(erroQuadratico);
+[~,melhorResultadoTaxaDeAcertos ] = max(taxaDeAcertos);
+
+% plot(1:21, erroQuadratico, 1:21, taxaDeAcertos*5*10^7);
+
+%=============FIM DE TAXAS DE ERROS E ACERTOS==============================
 
