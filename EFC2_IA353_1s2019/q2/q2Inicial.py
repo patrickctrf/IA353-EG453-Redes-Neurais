4391bcfa137557e941b02704fbd89660034421b2
patrickctrf <patrickctrf@hotmail.com.br>
2019-06-07 08:09:20 -0300

    Terminado e enviado

---
+++ EFC2_IA353_1s2019/q2/q2Inicial.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+import os
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+# reshape to be [samples][width][height][pixels]
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+ activation='relu',
+input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation='relu'))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.compile(optimizer='adam',
+ loss='sparse_categorical_crossentropy',
+ metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
+model_json = model.to_json()
+json_file = open("model_CNN.json", "w")
+json_file.write(model_json)
+json_file.close()
+model.save_weights("model_CNN.h5")
+print("Model saved to disk")
+os.getcwd()
