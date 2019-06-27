2ae505360568fbffbbcd38a2ae1b5dfc9625dc9e
patrickctrf <patrickctrf@gmail.com>
2019-06-27 15:27:35 -0300

    Enviando servidor boi lab

--- EFC3_IA353_1s2019/q4/q4.py
+++ EFC3_IA353_1s2019/q4/q4.py
@@ -8,4 +8,4 @@ textgen.reset()
 textgen.train_from_file(file_path, new_model=True, num_epochs=20,gen_epochs=10, word_level=True)
 textgen.generate(interactive=True, top_n=5)
 textgen.save('domcasmurro.hdf5')
-# fim
+
